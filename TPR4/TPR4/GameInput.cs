1b2d1bf95458818167450698b672459c376a99fd
Pandanom <stas110922@gmail.com>
2020-11-12 09:52:15 +0200

    done!

---
+++ TPR4/TPR4/GameInput.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPR4
+{
+    class GameInput
+    {
+        public double[][] Matrix { get; private set; }
+        public int M { get; private set; }
+        public int N { get; private set; }
+
+        public void GetValues()
+        {
+            string s = "";
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Enter M: ");
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out int d);
+                M = d;
+            } while (!flag);
+            do
+            {
+                Console.WriteLine("Enter N: ");
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out int d);
+                N = d;
+            } while (!flag);
+
+            Matrix = new double[N][];
+            for (int i = 0; i < N; i++)
+                Matrix[i] = new double[M];
+            Console.WriteLine("Set random matrix? (y/n)");
+            s = Console.ReadLine();
+            if (s == "y")
+                SetRandom();
+            else
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {                       
+                        do
+                        {
+                            Console.Write(string.Format("Enter a[{0}][{1}]: ",i,j ) );
+                            s = Console.ReadLine();
+                            flag = double.TryParse(s, out double d);
+                            Matrix[i][j] = d;
+                        } while (!flag);
+                    }
+                    Console.WriteLine("----------");
+                }
+            }
+        }
+
+        public void SetRandom()
+        {
+            Random r = new Random();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    Matrix[i][j] = r.Next(-10, 10);
+
+        }
+
+    }
+}
---
+++ TPR4/TPR4/MathGame.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPR4
+{
+    class MathGame
+    {
+        int m, n;
+        double[][] a;
+        double[] sAns;
+        double[] p;
+        double minValue;
+        public void Start()
+        {
+            GameInput gi = new GameInput();
+            gi.GetValues();
+            m = gi.M;
+            n = gi.N;
+            a = gi.Matrix;
+            p = new double[n];
+            Print();
+            FindAB();
+            Normalize();
+            Print();
+            Simplex();
+            FindValue();
+        }
+
+        void Print()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(a[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        void FindAB()
+        {
+            var maxI = new double[n];
+            var minJ = new double[m];
+
+            for (int i = 0; i < n; i++)
+            {
+                var max = a[i][0];
+                for (int j = 0; j < m; j++)
+                    max = max < a[i][j] ? a[i][j] : max;
+                maxI[i] = max;
+            }
+            for (int j = 0; j < m; j++)
+            {
+                var min = a[0][j];
+                for (int i = 0; i < n; i++)
+                    min = min > a[i][j] ? a[i][j] : min;
+                minJ[j] = min;
+            }
+            var lp = maxI.Min();
+            var hp = minJ.Max();
+            Console.WriteLine("a = " + lp.ToString() + " b = " + hp.ToString());
+            if (lp < hp)
+                throw (new Exception("a < b"));
+        }
+
+        void Normalize()
+        {
+            var min = a[0][0];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    min = min > a[i][j] ? a[i][j] : min;
+            if (min < 0)
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        a[i][j] = a[i][j] - min;
+            minValue = min;
+        }
+
+        void Simplex()
+        {
+            Equation e = new Equation(n);
+            for (int i = 0; i < n; i++)
+                e.SetRow(i, a[i], true, 1);
+            var f = new double[m];
+            for (int i = 0; i < m; i++)
+                f[i] = 1;
+            e.SetFunc(f, false);
+            e.Print();            
+            e.Canonize();
+            Console.WriteLine("Canonize:");
+            e.Print();
+            e.FindDbr();
+            e.PrintDBR();
+            e.Simplex();
+            e.PrintAnswer();
+            sAns = e.Answer;
+        }
+
+        void FindValue()
+        {
+            double sum = 0;
+            foreach (var d in sAns)
+                sum += d;
+            var v = 1 / sum;
+            Console.WriteLine("v = " + v.ToString());
+            Console.WriteLine("Unmodified v = " + (v + minValue).ToString());
+            Console.Write("S = [");
+            for (int i = 0; i < n; i++)
+            {
+                p[i] = sAns[i] * v;
+                Console.Write(p[i].ToString() + " ");
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
